2cc2e243823640b980f08336761cf4e50c7cfb67
Melvin jerry <+>
2024-07-25 07:30:34 +0300

    added css stylings and fixed the bugs with the create button

--- js/index.js
+++ js/index.js
@@ -1,28 +1,42 @@
-const name=document.getElementById('mon')
-const age= document.getElementById('age')
-const describe=document.getElementById('describe')
-const monsters=document.getElementById('monsters')
-const createBtn=document.getElementById('create')
+
+const monsters = document.getElementById("monsters");
+const createBtn = document.getElementById("create");
+const form1 = document.getElementById("forms");
+const monsters2=document.getElementById('monsters2')
 // to display the first 50 monsters
 
-fetch('http://localhost:3000/monsters')
-.then(res=>res.json())
-.then(data=>{
-    data.forEach(data => {
-        const list=document.createElement('li')
-        list.innerHTML=`
+fetch("http://localhost:3000/monsters")
+  .then((res) => res.json())
+  .then((data) => {
+    
+    data.forEach((data) => {
+      const list = document.createElement("li");
+      list.innerHTML = `
     <h2 id="name"> Name: ${data.name}</h2>
 <h2 id="age">Age:${data.age}</h2>
-<h4 id="description">description:${data.description}</h4>`
+<h4 id="description">description:${data.description}</h4>`;
 
-monsters.appendChild(list)
+      monsters.appendChild(list);
     });
-})
-
-function createMonster(params) {
+  });
+// get form data
+createBtn.addEventListener("click", () => {
     
+const formData={
+    name:document.getElementById('mon').value,
+    age:document.getElementById('age').value,
+    description:document.getElementById('describe').value
 }
+fetch('http://localhost:3000/monsters',{
+    method:'POST',
+    headers:{
+        'Content-Type':'application/json'
+    },
+    body:JSON.stringify(formData)
+})
+.then(res=>res.json())
+.then(FormData=>
+monsters.appendChild(formData))
 
 
-// createMonster function
-
+})
